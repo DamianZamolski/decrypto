fdd63b1c2298ebd486288e9ec5b234ac3e70ddfc
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2024-11-10 14:16:37 +0100

    fix tests

--- test/tests/example.spec.ts
+++ test/tests/example.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 import { fail } from 'assert';
 
-test('has title', async ({ page }) => {
+test(`has 'imprezówki' title`, async ({ page }) => {
   const webUrl = process.env.WEB_URL;
   if (!webUrl) {
     fail();
   }
   await page.goto(webUrl);
-  await expect(page).toHaveTitle(/next/i);
+  await expect(page).toHaveTitle(/imprezówki/i);
 });
